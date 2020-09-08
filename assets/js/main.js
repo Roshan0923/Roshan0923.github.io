e166ea3aa0df4fba80aeb718dcdd68d233b40ff7
Roshan Kirtikumar Patel <rs735202@dal.ca>
2020-09-07 22:29:03 -0300

    Created porfolio website

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,13 +25,13 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
-});
+// /*===== SCROLL REVEAL ANIMATION =====*/
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '80px',
+//     duration: 2000,
+//     reset: true
+// });
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
